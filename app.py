eba296a48455bc66bad9e2c1fd4b33ff4ecb9511
chaayagirimon <chaaya.girimon@gmail.com>
2021-06-17 14:32:26 +0530

    2 files are added

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
-print("hello")
+'''
+Created on 
+
+Course work: 
+
+@author: raja
+
+Source:
+    
+'''
+
+# Import necessary modules
+ 
+
+
+def remove_duplicates(city_list):
+
+    non_duplicate_cities = []
+
+    for x in city_list:
+        if x not in non_duplicate_cities:
+            non_duplicate_cities.append(x)
+    return non_duplicate_cities
+
+def startpy():
+
+    c_list = [
+        "Chennai",
+        "Hyderabad",
+        "Montreal",
+        "Chennai",
+        "Montreal"
+    ]
+
+    unique_cities = remove_duplicates(c_list)
+   
+    print(unique_cities)
+
+
+if __name__ == '__main__':
+    startpy()
---
+++ random_names.py
@@ -0,0 +1,29 @@
+'''
+Created on 
+
+Course work: 
+
+@author: raja
+
+Source:
+    
+'''
+
+from faker import Faker
+ 
+fake=Faker()
+
+def generate_random_names():
+
+    random_names = []
+    # for x in range 
+    print(fake.name())
+    pass
+
+def startpy():
+
+ generate_random_names()   
+
+
+if __name__ == '__main__':
+    startpy()
