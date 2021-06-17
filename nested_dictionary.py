c74d6ca2bd351a1add6addbb9a19e993da5d7f2c
chaayagirimon <chaaya.girimon@gmail.com>
2021-06-17 16:56:50 +0530

    nested dictionary

---
+++ nested_dictionary.py
@@ -0,0 +1,32 @@
+'''
+Created on 
+
+Course work: 
+
+@author: raja
+
+Source:
+     
+     
+'''
+
+def startpy():
+    nest_dict = {
+        "name"      : "vedha",
+        "list1"     : [1,2,3],
+        "dict1"     : {
+            "age"       :23,
+            "age1"      :21
+        },
+        "list2"     : [4,5,6],
+        "dict2"     : {
+            "vedha"       :18,
+            "deeksha"     :21
+        }
+    }
+    print(nest_dict)
+ 
+
+
+if __name__ == '__main__':
+    startpy()
